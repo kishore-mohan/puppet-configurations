8249fd0cefc9627bed8deda379e64b44f14e3709
puppetserver <puppetserver@puppet.example.com>
2015-02-25 19:40:38 +0530

    master configurations

--- master/puppet/manifests/site.pp
+++ master/puppet/manifests/site.pp
@@ -2,6 +2,60 @@ node 'node01.example.com' {
 include helloworld
 }
 
-node 'node02.example.com'{
+node default {
 include hellopuppet
+exec {'remotedesktop':
+	command => 'C:\Windows\System32\netsh.exe advfirewall
+	firewall set rule froup="remote desktop" new enable=Yes',
+	refreshonly => true,
+} 
+
+exec {'zabbixagent':
+	command => 'C:\Windows\System32\netsh.exe advfirewall
+firewall and rule name="Zabbix Agent" dir=in action=allow protocol=
+TCP localport=10050',
+	refreshonly => true,
+}
+
+#include windows-iis::firewall
+#include windows-iis::files
+
+  Dism { ensure => present, }
+  dism { 'IIS-WebServerRole': } ->
+  dism { 'IIS-WebServer': }
+
+#dotnet { 'dotnet45': version => '4.5' }
+vcsrepo { "C:/nopcommerce":
+  ensure   => latest,
+  provider => git,
+  source   => 'git@github.com:kishore-mohan/nopcommerce-puppet.git',
+  revision => 'master',
+}
+
+
+iis_apppool {'nopcommerce':
+	ensure => present,
+	managedpipelinemode => 'Integrated',
+	managedruntimeversion => 'v4.0',
+}
+
+iis_site {'nopcommerce':
+	ensure => present,
+	bindings => ["http/*:3000:"],
+}
+
+iis_app {'nopcommerce/':
+	ensure => present,
+	applicationpool => 'nopcommerce',
+}
+
+iis_vdir {'nopcommerce/':
+	ensure => present,
+	iis_app => 'nopcommerce/',
+	physicalpath => 'C:\nopcommerce',
+}
+
+
+
+
 }
---
+++ master/puppet/modules/dotnet/spec/defines/dotnet_spec.rb
@@ -0,0 +1,40 @@
+require 'spec_helper'
+
+describe 'dotnet', :type => :define do
+
+  ['Windows Server 2012','Windows Server 2008 R2','Windows Server 2008', 'Windows Server 2003','Windows Server 2003 R2','Windows 8','Windows 7','Windows Vista','Windows XP'].each do |os|
+    context "with invalid custom param: os => #{os}, version => fubar" do
+      let :facts do
+        { :operatingsystemversion => os }
+      end
+      let :title do 'fubar' end
+      let :params do
+        { :version => 'fubar' }
+      end
+
+      it do
+        expect {
+          should contain_exec('install-dotnet-3.5')
+        }.to raise_error(Puppet::Error)
+      end
+    end
+  end
+
+  ['Windows Server 2012','Windows Server 2008 R2','Windows Server 2008', 'Windows Server 2003','Windows Server 2003 R2','Windows 8','Windows 7','Windows Vista','Windows XP'].each do |os|
+    context "with invalid custom param: os => #{os}, ensure => fubar" do
+      let :facts do
+        { :operatingsystemversion => os }
+      end
+      let :title do 'fubar' end
+      let :params do
+        { :version => '3.5', :ensure => 'fubar' }
+      end
+
+      it do
+        expect {
+          should contain_exec('install-dotnet-3.5')
+        }.to raise_error(Puppet::Error)
+      end
+    end
+  end
+end
